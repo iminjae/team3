17fcd2704c8631e4dbc59a8a84812da72ba1959a
dayeo <dayeong815@gmail.com>
2021-06-21 17:00:25 +0900

    7. 왕다영머지 테스트

--- src/team3/com/kh/codingTest/test.java
+++ src/team3/com/kh/codingTest/test.java
@@ -1,9 +1,39 @@
 package team3.com.kh.codingTest;
 
 public class test {
-	//이민재 테스트
-	//장성현 테스트
-  //김명선 테스트
+    public static void a2557() {
+  		System.out.println("Hello World!");
+  	}
+      
+      public static void a10718() {
+           System.out.println("강한친구 대한육군");
+           System.out.println("강한친구 대한육군");
+  	}
+      
+      public static void a10171() {
+      	System.out.println("\\    /\\");
+      	System.out.println(") )  ( ')");
+      	System.out.println("(  /  ) ");
+      	System.out.println(" \\(__)|");
+      	
+      }
+      
+      
+      public static void a10172() {
+      	System.out.println("|\\_/|");
+      	System.out.println("|q p|   /}");
+      	System.out.println("( 0 )\"\"\"\\");
+      	System.out.println("|\"^\"`    |");
+      	System.out.println("||_/=\\\\__|");
+      }
+      
+      
+      public static void name() {
+    	  
+    	  System.out.println("3팀");
+    	  
+    	  
+      }
 }
 
 
