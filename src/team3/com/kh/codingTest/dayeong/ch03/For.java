c623b16c211361714535071ea68eb22406b4813b
dayeo <dayeong815@gmail.com>
2021-06-21 16:18:53 +0900

    6.for문 실습문제(왕다영)

--- src/team3/com/kh/codingTest/dayeong/ch03/For.java
+++ src/team3/com/kh/codingTest/dayeong/ch03/For.java
@@ -1,10 +1,16 @@
 package team3.com.kh.codingTest.dayeong.ch03;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class For {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //throws IOException
 		Scanner in = new Scanner(System.in);
 //		int dan = in.nextInt();
 //		if (dan >= 1 && dan <= 9) {
@@ -33,16 +39,43 @@ public class For {
 //	
 //	
 
+//		int n = in.nextInt();
+//		int sum = 0;
+//
+//		for (int i = 1; i <= n; i++) {
+//
+//			sum += i;
+//		}
+//
+//		System.out.println(sum);
+
+//		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        int n = Integer.parseInt(bf.readLine());
+//        for(int i = 0; i < n; i++){
+//            String s = bf.readLine();
+//            int a = Integer.parseInt(s.split(" ")[0]);
+//            int b = Integer.parseInt(s.split(" ")[1]);
+//            bw.write(a+b+"\n");
+//        }
+//        bw.flush();
+
+//		
+//		int n = in.nextInt();
+//		for(int i = 1; i <= n ; i ++) {
+//			System.out.println(i);
+//		}
+
+		
 		int n = in.nextInt();
-		int sum = 0;
-
-		for (int i = 1; i <= n; i++) {
-
-			sum += i;
+		for(int i = n; i >= 1 ; i--) {
+			System.out.println(i);
 		}
-
-		System.out.println(sum);
-
+		
+		
+		
+		
+		
 	}
 
 }
