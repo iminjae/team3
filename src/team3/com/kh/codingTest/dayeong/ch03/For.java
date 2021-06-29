dc6c40e89153e3d4ac3beaf61199f07c58fbc914
dayeo <dayeong815@gmail.com>
2021-06-29 13:31:14 +0900

    10.백준 for문 끝까지(왕다영)

--- src/team3/com/kh/codingTest/dayeong/ch03/For.java
+++ src/team3/com/kh/codingTest/dayeong/ch03/For.java
@@ -9,15 +9,77 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class For {
-	
+
 	public static void q11021() {
-		
-		
-		
+		Scanner in = new Scanner(System.in);
+
+		int a = in.nextInt();
+
+		for (int i = 1; i <= a; i++) {
+			int b = in.nextInt();
+			int c = in.nextInt();
+
+			System.out.println("Case #" + i + ": " + (b + c));
+		}
+
+		in.close();
+
 	}
 
-	public static void main(String[] args) { //  throws IOException
-		//Scanner in = new Scanner(System.in);
+	public static void q11022() {
+		Scanner in = new Scanner(System.in);
+
+		int a = in.nextInt();
+
+		for (int i = 1; i <= a; i++) {
+			int b = in.nextInt();
+			int c = in.nextInt();
+
+			System.out.println("Case #" + i + ": " + b + " + " + c + " = " + (b + c));
+		}
+
+		in.close();
+
+	}
+
+	public static void q2438() {
+		Scanner in = new Scanner(System.in);
+
+		int a = in.nextInt();
+
+		for (int i = 1; i <= a; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+
+		}
+	}
+
+	public static void q2439() {
+		Scanner in = new Scanner(System.in);
+
+		int N = in.nextInt();
+		in.close();
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k < i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) { // throws IOException
+		// q11021();
+		// q11022();
+		// q2438();
+		q2439();
+
+//		Scanner in = new Scanner(System.in);
 //		int dan = in.nextInt();
 //		if (dan >= 1 && dan <= 9) {
 //			for (int i = 1; i <= 9; i++) {
@@ -55,42 +117,39 @@ public class For {
 //
 //		System.out.println(sum);
 
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		try {
-	
-        int n;
-		
-			n = Integer.parseInt(bf.readLine());
-			
-			 for(int i = 0; i < n; i++){
-		            String s = bf.readLine();
-		            int a = Integer.parseInt(s.split(" ")[0]);
-		            int b = Integer.parseInt(s.split(" ")[1]);
-		            bw.write(a+b+"\n");
-			 }   
-			 
-			 bw.flush();
-		            
-		} catch (NumberFormatException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally {
-	        
-	        try {
-				bw.close();
-				bf.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-       
-        
-        
-        
-        
+//		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//		try {
+//	
+//        int n;
+//		
+//			n = Integer.parseInt(bf.readLine());
+//			
+//			 for(int i = 0; i < n; i++){
+//		            String s = bf.readLine();
+//		            int a = Integer.parseInt(s.split(" ")[0]);
+//		            int b = Integer.parseInt(s.split(" ")[1]);
+//		            bw.write(a+b+"\n");
+//			 }   
+//			 
+//			 bw.flush();
+//		            
+//		} catch (NumberFormatException | IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}finally {
+//	        
+//	        try {
+//				bw.close();
+//				bf.close();
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+//		
+//        
+//        
 //		
 //		int n = in.nextInt();
 //		for(int i = 1; i <= n ; i ++) {
@@ -103,10 +162,7 @@ public class For {
 //			System.out.println(i);
 //		}
 //		
-		
-		
-		
-		
+
 	}
 
 }
